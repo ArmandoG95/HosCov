b17834a52d3a04b7093018fad2635eeb0dcf8747
EMIGDIO ESPINOSA JASSO <millosespinosa@gmail.com>
2021-02-12 12:50:27 -0600

    Creación de logo Hoscov

---
+++ HosCovPro/FrmRegVacuna.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HosCovPro
+{
+    public partial class FrmRegVacuna : Form
+    {
+        public FrmRegVacuna()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmRegVacuna_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
