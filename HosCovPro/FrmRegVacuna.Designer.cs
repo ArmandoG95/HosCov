1a8166f45d5d12ab149ca75330661ef9688a5da7
EMIGDIO ESPINOSA JASSO <millosespinosa@gmail.com>
2021-02-15 17:32:26 -0600

    Agregue combo box con el nombre Datos Personales y una Label con nombre Nombre y apellidos

--- HosCovPro/FrmRegVacuna.Designer.cs
+++ HosCovPro/FrmRegVacuna.Designer.cs
@@ -30,7 +30,10 @@
         {
             this.pctLogo = new System.Windows.Forms.PictureBox();
             this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.grpDatosPersonales = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.pctLogo)).BeginInit();
+            this.grpDatosPersonales.SuspendLayout();
             this.SuspendLayout();
             // 
             // pctLogo
@@ -50,9 +53,29 @@
             this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitulo.Location = new System.Drawing.Point(294, 17);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(1026, 39);
+            this.lblTitulo.Size = new System.Drawing.Size(984, 38);
             this.lblTitulo.TabIndex = 1;
             this.lblTitulo.Text = "Bienvenido al registro para la vacuna de covid del hospital HosCov";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(22, 29);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(129, 17);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre y apellidos";
+            this.lblNombre.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // grpDatosPersonales
+            // 
+            this.grpDatosPersonales.Controls.Add(this.lblNombre);
+            this.grpDatosPersonales.Location = new System.Drawing.Point(16, 179);
+            this.grpDatosPersonales.Name = "grpDatosPersonales";
+            this.grpDatosPersonales.Size = new System.Drawing.Size(419, 471);
+            this.grpDatosPersonales.TabIndex = 3;
+            this.grpDatosPersonales.TabStop = false;
+            this.grpDatosPersonales.Text = "Datos Personales";
             // 
             // FrmRegVacuna
             // 
@@ -60,12 +83,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightBlue;
             this.ClientSize = new System.Drawing.Size(1332, 891);
+            this.Controls.Add(this.grpDatosPersonales);
             this.Controls.Add(this.lblTitulo);
             this.Controls.Add(this.pctLogo);
             this.Name = "FrmRegVacuna";
             this.Text = "FrmRegVacuna";
             this.Load += new System.EventHandler(this.FrmRegVacuna_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pctLogo)).EndInit();
+            this.grpDatosPersonales.ResumeLayout(false);
+            this.grpDatosPersonales.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -75,5 +101,7 @@
 
         private System.Windows.Forms.PictureBox pctLogo;
         private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.GroupBox grpDatosPersonales;
     }
 }
